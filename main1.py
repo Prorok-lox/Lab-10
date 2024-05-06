1d40e09a500267943b1fddf9ec0aa9b3eb3e92fd
ILYA <il_vlad@inbox.ru>
2024-05-06 13:16:30 +0300

    final

--- main1.py
+++ main1.py
@@ -2,6 +2,9 @@ import json, time
 
 import pyttsx3, pyaudio, vosk
 
+import requests
+from random import randint
+from PIL import Image
 
 
 class Speech:
@@ -27,18 +30,17 @@ class Speech:
 
 class Recognize:
     def __init__(self):
-        model = vosk.Model('model_small')
+        model = vosk.Model('vosk-model-small-ru-0.4')
         self.record = vosk.KaldiRecognizer(model, 16000)
         self.stream()
 
     def stream(self):
         pa = pyaudio.PyAudio()
         self.stream = pa.open(format=pyaudio.paInt16,
-                         channels=1,
-                         rate=16000,
-                         input=True,
-                         frames_per_buffer=8000)
-
+                              channels=1,
+                              rate=16000,
+                              input=True,
+                              frames_per_buffer=8000)
 
     def listen(self):
         while True:
@@ -54,6 +56,50 @@ def speak(text):
     speech.text2voice(speaker=1, text=text)
 
 
+def rand():
+    url = f"https://rickandmortyapi.com/api/character/{randint(1, 108)}"
+    resp = requests.get(url).json()
+    name = resp['name']
+    return name
+
+
+def download():
+    global image_counter
+    url = f"https://rickandmortyapi.com/api/character/{randint(1, 108)}"
+    resp = requests.get(url).json()
+    path = resp['image']
+    data = requests.get(path).content
+    with open(f'image{image_counter}.jpeg', 'wb') as f:
+        f.write(data)
+        f.close()
+    image_counter += 1
+
+
+def episode():
+    url = f"https://rickandmortyapi.com/api/character/{randint(1, 108)}"
+    resp = requests.get(url).json()
+    name = resp['name']
+    ep = requests.get(resp['episode'][0]).json()
+    title = ep['name']
+    return f"{name} first appearance was in episode \"{title}\""
+
+
+def show_image():
+    global image_counter
+    download()
+    image = Image.open(f'image{image_counter - 1}.jpeg')
+    image.show()
+
+
+def resolution():
+    global image_counter
+    download()
+    image = Image.open(f'image{image_counter - 1}.jpeg')
+    w, h = image.size
+    return f'Resolution of image{image_counter - 1}.jpeg is {w} pixels by {h} pixels'
+
+
+image_counter = 0
 rec = Recognize()
 text_gen = rec.listen()
 rec.stream.stop_stream()
@@ -62,7 +108,20 @@ time.sleep(0.5)
 rec.stream.start_stream()
 for text in text_gen:
     if text == 'закрыть':
-        speak('Бывай, ихтиандр')
+        speak('So long, ichthyander.')
         quit()
+    elif text == 'случайный':
+        speak(rand())
+    elif text == 'сохранить':
+        download()
+        speak('Successfully downloaded picture of random character to image.jpeg file')
+    elif text == 'эпизод':
+        speak(episode())
+    elif text == 'показать':
+        speak('Now I show you the picture of random character')
+        show_image()
+    elif text == 'разрешение':
+        speak(resolution())
     else:
+        speak('Sorry, I don\'t know this command')
         print(text)
